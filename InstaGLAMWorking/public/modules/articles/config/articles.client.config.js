ef44a307f47b7bad9889d0d600cbe4ad56c4229b
Anna Kimball <kimball.anna@knights.ucf.edu>
2015-05-04 07:46:33 -0400

    Article

--- InstaGLAMWorking/public/modules/articles/config/articles.client.config.js
+++ InstaGLAMWorking/public/modules/articles/config/articles.client.config.js
@@ -8,5 +8,5 @@ angular.module('articles').run(['Menus',
 		/*Menus.addMenuItem('topbar', 'Selfie Map', 'articles', 'dropdown', '/articles(/create)?');
 		Menus.addSubMenuItem('topbar', 'articles', 'Your Selfie Locations', 'articles');
 		Menus.addSubMenuItem('topbar', 'articles', 'New Selfie Location', 'articles/create');
-	}*/
+	*/}
 ]);
