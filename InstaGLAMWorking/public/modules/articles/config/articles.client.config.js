d7abdf0122edd97a7aa82bdef3e3e484397c15ea
Anna Kimball <kimball.anna@knights.ucf.edu>
2015-05-04 07:52:34 -0400

    Map

--- InstaGLAMWorking/public/modules/articles/config/articles.client.config.js
+++ InstaGLAMWorking/public/modules/articles/config/articles.client.config.js
@@ -5,8 +5,8 @@ angular.module('articles').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		
-		/*Menus.addMenuItem('topbar', 'Selfie Map', 'articles', 'dropdown', '/articles(/create)?');
+		Menus.addMenuItem('topbar', 'Selfie Map', 'articles', 'dropdown', '/articles(/create)?');
 		Menus.addSubMenuItem('topbar', 'articles', 'Your Selfie Locations', 'articles');
 		Menus.addSubMenuItem('topbar', 'articles', 'New Selfie Location', 'articles/create');
-	*/}
+	}
 ]);
